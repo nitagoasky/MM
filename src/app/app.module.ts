eb2eeddc3d04f14e7a2fc9fabd0fdca6a51f3e3c
GaoTian <1139592884@qq.com>
2017-11-04 09:10:53 +0800

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {PasswordModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
