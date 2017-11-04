1eefd3511dcb38030184473e08562b2a3197a669
GaoTian <1139592884@qq.com>
2017-11-04 09:50:11 +0800

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {PasswordModule} from 'primeng/primeng';
+import { FormsModule } from '@angular/forms';
+import {PasswordModule, InputTextModule, ButtonModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,10 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    PasswordModule
+    FormsModule,
+    PasswordModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username: string;
   constructor() { }
   ngOnInit() {
+  }
+  login() {
+    console.log(this.username);
   }
 
 }
